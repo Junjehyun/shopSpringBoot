dfc315bb827823ddbc5ed8f17782f32413da537e
Junjehyun <h94051987@gmail.com>
2023-03-29 14:31:25 +0900

    230329

--- shop/src/main/java/com/shop/entity/Item.java
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -22,8 +22,12 @@ public class Item {
 
     @Column(nullable = false, length = 50)
     private String ItemNm; // 상품명
+    @Column(name="price", nullable = false)
     private int price; // 가격
+    @Column(nullable = false)
     private int stockNumber; // 재고수량
+    @Lob
+    @Column(nullable = false)
     private String itemDetail; // 상품 상세 설명
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; // 상품 판매 상태
--- shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.shop.repository;
 
+
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -20,9 +20,9 @@ class ItemRepositoryTest {
 
     @Test
     @DisplayName("상품 저장 테스트")
-    public void createItemTest(){
+    public void createItemTest() {
         Item item = new Item();
-        item.setItemNm("테스트 상품");
+        item.setItemNm("테스트상품");
         item.setPrice(10000);
         item.setItemDetail("테스트 상품 상세 설명");
         item.setItemSellStatus(ItemSellStatus.SELL);
@@ -31,29 +31,6 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
-
     }
 
-    public void createItemList() {
-    for(int i=1; i<=10; i++) {
-        Item item = new Item();
-        item.setItemNm("테스트 상품" + i);
-        item.setPrice(10000 + i);
-        item.setItemDetail("테스트 상품 상세 설명" + i);
-        item.setItemSellStatus(ItemSellStatus.SELL);
-        item.setStockNumber(100);
-        item.setRegTime(LocalDateTime.now());
-        item.setUpdateTime(LocalDateTime.now());
-        Item savedItem = itemRepository.save(item);
-    }
-    }
-    @Test
-    @DisplayName("상품명 조회 테스트")
-    public void findByItemNmTest(){
-        this.createItemList();
-        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
-        for(Item item : itemList){
-            System.out.println(item.toString());
-        }
-    }
 }
