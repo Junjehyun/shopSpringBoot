7b9a281a75e529b7ef8633250ff056d03efb4f94
ジェヒョン <h94051987@gmail.com>
2023-11-01 10:45:00 +0900

    231101

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,9 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,7 +12,10 @@ import java.util.List;
 // 첫 번째는 엔티티 타입 클래스를 넣어주고, 두 번째는 기본키 타입을 넣어준다. Item 클래스는 기본키 타입이 Long이므로
 // Long을 넣어준다. JpaRepository는 기본적은 CRUD 및 페이징 처리를 위한 메소드가 정의 되어 있다.
 // 예제에서 작성할 테스트 코드는 엔티티를 저장하는 save() 메소드다.
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>,
+QuerydslPredicateExecutor<Item> // QueryDslPredicateExecutor 인터페이스 상속을 추가
+
+{
 
     List<Item> findByItemNm(String itemNm);
     // itemNm(상품명)으로 데이터를 조회하기 위해서 By 뒤에 필드명인 ItemNm을 메소드의 이름에 붙여준다.
@@ -22,5 +28,20 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByPriceLessThan(Integer price);
     // 파라미터로 넘어온 price 변수보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드.
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer Price);
+
+    // @Query를 이용한 검색 처리하기
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    // @Query 어노테이션 안에 JPQL로 작성한 쿼리문을 넣어준다. from 뒤에는 엔티티 클래스로 작성한 Item을 지정했고,
+    // Item으로부터 데이터를 Select 하겠다는 것을 의미한다.
+
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+    // 파라미터에 @Param 어노테이션을 이용하여 파라미터로 넘어온 값을 JPQL에 들어갈 변수로 지정할 수 있음.
+    // 현재는 itemDetail변수를 "like % %" 사이에 ":ItemDetail"로 값이 들어가도록 작성했다.
+
+    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc"
+    , nativeQuery = true) // value안에 네이티브 쿼리문을 작성하고 "nativeQuery=true"를 지정한다.
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
 }
